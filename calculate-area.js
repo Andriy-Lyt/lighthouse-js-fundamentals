cb3cf57d0080f502923926cdb46a0adfa291c8bd
Andriy-Lyt <howdy_eh@icloud.com>
2021-06-27 21:44:47 +0000

    arrays more

---
+++ calculate-area.js
@@ -0,0 +1,25 @@
+
+function calculateRectangleArea(length, width){
+  let area = length * width;
+  return area >= 0 ? area : undefined;
+}
+function calculateTriangleArea(base, height){
+  let area = base * height / 2;
+  return area >= 0 ? area : undefined;
+}
+function calculateCircleArea(radius){
+  let area = Math.PI * Math.pow(radius, radius);
+  return area >= 0 ? area : undefined;
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
